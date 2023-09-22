3c097d27c2cd035609ab5548b5112b68c1c6c48b
Vajeeha Mohammed <vajeehamohammed@jmangroup.com>
2023-09-22 19:05:06 +0530

    api s

---
+++ Backend/models/techstack.js
@@ -0,0 +1,6 @@
+const techStackSchema = new mongoose.Schema({
+    techStack: String,
+  });
+  
+  // Create a model for the tech stack options
+  const TechStack = mongoose.model('TechStack', techStackSchema);
