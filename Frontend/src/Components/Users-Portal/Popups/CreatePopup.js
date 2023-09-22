75d38d20a7f564349d5cf5d9c7d3825903871c0b
Vajeeha Mohammed <vajeehamohammed@jmangroup.com>
2023-09-22 19:36:56 +0530

    filter

--- Frontend/src/Components/Users-Portal/Popups/CreatePopup.js
+++ Frontend/src/Components/Users-Portal/Popups/CreatePopup.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './Popup.css';
 import Select from 'react-select';
 import DatePicker from 'react-datepicker';
@@ -17,7 +17,6 @@ function CreatePopup({ onSave, onClose, initialData }) {
   });
 
   useEffect(() => {
-    // Update the newRowData state when initialData changes (e.g., when reopening the popup)
     setNewRowData(initialData || {
       projectName: '',
       projectManagerName: '',
@@ -28,11 +27,11 @@ function CreatePopup({ onSave, onClose, initialData }) {
     });
   }, [initialData]);
 
-  const statusOptions = [
+  const statusOptions = useMemo(() => [
     { label: 'In Progress', value: 'inprogress' },
     { label: 'Completed', value: 'completed' },
     { label: 'Pending', value: 'pending' },
-  ];
+  ], []); // Memoize the statusOptions array
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -48,7 +47,6 @@ function CreatePopup({ onSave, onClose, initialData }) {
   };
 
   const handleCreate = () => {
-    // Check if all required fields are filled
     if (
       newRowData.projectName &&
       newRowData.projectManagerName &&
@@ -59,29 +57,24 @@ function CreatePopup({ onSave, onClose, initialData }) {
       const startTime = new Date(newRowData.startTime);
       const endTime = new Date(newRowData.endTime);
 
-      // Check if the end date is greater than or equal to the start date
       if (startTime <= endTime) {
-        // Prepare the data to send to the backend
         const dataToSend = {
           projectName: newRowData.projectName,
           projectManagerName: newRowData.projectManagerName,
-          status: newRowData.status.value, // Use .value to get the selected value from the Select component
+          status: newRowData.status.value,
           startTime: startTime.toISOString(),
           endTime: endTime.toISOString(),
           techStack: newRowData.techStack,
         };
 
-        // Send data to the backend API
         axios
           .post('http://localhost:8080/api/auth/createRow', dataToSend)
           .then((response) => {
-            // Handle successful response
             console.log('Data saved successfully:', response.data);
             onSave(newRowData);
             onClose();
           })
           .catch((error) => {
-            // Handle error
             console.error('Error while creating data:', error);
             alert('Error while saving data. Please try again later.');
           });
@@ -94,7 +87,7 @@ function CreatePopup({ onSave, onClose, initialData }) {
   };
 
   return (
-    <div className="popup" style={{borderRadius:"8px"}} >
+    <div className="popup" style={{ borderRadius: "8px" }}>
       <h2>Create New Row</h2>
       <form>
         <div className="form-group">
@@ -105,7 +98,7 @@ function CreatePopup({ onSave, onClose, initialData }) {
             name="projectName"
             value={newRowData.projectName}
             onChange={handleInputChange}
-            required // Add HTML5 validation for required field
+            required
           />
         </div>
         <div className="form-group">
@@ -116,32 +109,31 @@ function CreatePopup({ onSave, onClose, initialData }) {
             name="projectManagerName"
             value={newRowData.projectManagerName}
             onChange={handleInputChange}
-            required // Add HTML5 validation for required field
+            required
           />
         </div>
-        <div style={{display:"grid",gridTemplateColumns:"1fr 1fr",gap:"40px"}} >
-        <div className="form-group">
-          <label htmlFor="startTime">Start Date</label>
-          <DatePicker
-            id="startTime"
-            selected={newRowData.startTime}
-            onChange={(date) => handleDateChange('startTime', date)}
-            dateFormat="MMMM d, yyyy"
-            required // Add HTML5 validation for required field
-          />
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "40px" }}>
+          <div className="form-group">
+            <label htmlFor="startTime">Start Date</label>
+            <DatePicker
+              id="startTime"
+              selected={newRowData.startTime}
+              onChange={(date) => handleDateChange('startTime', date)}
+              dateFormat="MMMM d, yyyy"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="endTime">End Date</label>
+            <DatePicker
+              id="endTime"
+              selected={newRowData.endTime}
+              onChange={(date) => handleDateChange('endTime', date)}
+              dateFormat="MMMM d, yyyy"
+              required
+            />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="endTime">End Date</label>
-          <DatePicker
-            id="endTime"
-            selected={newRowData.endTime}
-            onChange={(date) => handleDateChange('endTime', date)}
-            dateFormat="MMMM d, yyyy"
-            required // Add HTML5 validation for required field
-          />
-        </div>
-        </div>
-
         <div className="form-group">
           <label htmlFor="status">Status</label>
           <Select
@@ -150,7 +142,7 @@ function CreatePopup({ onSave, onClose, initialData }) {
             value={newRowData.status}
             options={statusOptions}
             onChange={handleStatusChange}
-            required // Add HTML5 validation for required field
+            required
           />
         </div>
         <div className="form-group">
@@ -164,10 +156,10 @@ function CreatePopup({ onSave, onClose, initialData }) {
           />
         </div>
         <div className="form-actions  ">
-          <Button variant='outlined'  onClick={onClose}>
+          <Button variant='outlined' onClick={onClose}>
             Close
           </Button>
-          <Button variant='contained'  onClick={handleCreate}>
+          <Button variant='contained' onClick={handleCreate}>
             Create
           </Button>
         </div>
